ad12f071a1cf5b5df1d814b00de5149adb68e159
Ken Rutan and Susha Dore <susha@>
2017-04-20 13:35:17 -0700

    add filter that removes even numbers.

--- lib/prime_numbers.rb
+++ lib/prime_numbers.rb
@@ -1,9 +1,19 @@
 class Fixnum
   define_method(:prime_numbers) do
-    array=[]
+    array = []
+    primes = [2]
     (self-1).times do |time|
       array.push(time+2)
     end
+    array.each do |item|
+      if item % 2 == 0
+        array.delete(item)
+      end
+    end
+
+
+
+    primes.concat(array)
     array
   end
 end
--- spec/prime_number_spec.rb
+++ spec/prime_number_spec.rb
@@ -4,6 +4,9 @@ require('pry')
 
 describe("Fixnum#prime_numbers") do
   it("takes input value and returns an array which starts with 2 and ends with the input value") do
-    expect(3.prime_numbers()).to(eq([2, 3]))
+    expect(3.prime_numbers()).to(eq([3]))
+  end
+  it("takes mutltiples of two and removes them from the prime number array") do
+    expect(6.prime_numbers()).to(eq([3, 5]))
   end
 end
